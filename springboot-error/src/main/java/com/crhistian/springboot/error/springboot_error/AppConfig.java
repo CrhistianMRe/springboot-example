032519d3da32a500dc131f982bf3c29c5b8bcaba
CrhistianMRe <came.29meca@gmail.com>
2025-05-20 18:27:38 +0800

    Add Bean component with arraylist for di in impl

--- springboot-error/src/main/java/com/crhistian/springboot/error/springboot_error/AppConfig.java
+++ springboot-error/src/main/java/com/crhistian/springboot/error/springboot_error/AppConfig.java
@@ -1,5 +1,31 @@
 package com.crhistian.springboot.error.springboot_error;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.crhistian.springboot.error.springboot_error.models.domain.User;
+
+@Configuration
 public class AppConfig {
+
+
+    @Bean
+    List<User> users(){
+
+
+        List<User> users = new ArrayList<User>();
+        users.add(new User(1L, "Pepe", "Gonzales"));
+        users.add(new User(2L, "Andres", "Mena"));
+        users.add(new User(3L, "Maria", "Perez"));
+        users.add(new User(4L, "Josefa", "Ramirez"));
+        users.add(new User(5L, "Ale", "Gutierrez"));
+        
+        return users;
+    }
+    
 
 }
