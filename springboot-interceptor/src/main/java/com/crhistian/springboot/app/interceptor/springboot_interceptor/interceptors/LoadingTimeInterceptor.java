e131b65dd563558ce7995b07d0b472d94e0d8191
CrhistianMRe <came.29meca@gmail.com>
2025-05-24 13:19:10 +0800

    feat(interceptor): add handler method name to interceptors on terminal

--- springboot-interceptor/src/main/java/com/crhistian/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot-interceptor/src/main/java/com/crhistian/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,13 +20,14 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("LoadingTimeInterceptor: postHandle() Exiting.....");
+        logger.info("LoadingTimeInterceptor: postHandle() Exiting....." + ((HandlerMethod)handler).getMethod().getName());
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        logger.info("LoadingTimeInterceptor: preHandle() Entering.....");
+        HandlerMethod method = ((HandlerMethod)handler);
+        logger.info("LoadingTimeInterceptor: preHandle() Entering....." + method.getMethod().getName());
     
 
         return true;
