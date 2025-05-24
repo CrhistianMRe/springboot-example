9d23b26d933c7ed48c114827bc1f17ceb4a0ff16
CrhistianMRe <came.29meca@gmail.com>
2025-05-24 13:33:28 +0800

    feat(interceptor): add random delay to output on terminal in interceptor handler methods

--- springboot-interceptor/src/main/java/com/crhistian/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot-interceptor/src/main/java/com/crhistian/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.crhistian.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -18,20 +20,38 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("LoadingTimeInterceptor: postHandle() Exiting....." + ((HandlerMethod)handler).getMethod().getName());
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
+        throws Exception {
         HandlerMethod method = ((HandlerMethod)handler);
         logger.info("LoadingTimeInterceptor: preHandle() Entering....." + method.getMethod().getName());
-    
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
 
         return true;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+            
+
+        long end = System.currentTimeMillis();
+
+        long start = (long) request.getAttribute("start");
+
+        long result = end -start;
+
+        logger.info("Tiempo transcurrido "+  result +  " milisegundos!");
+
+        logger.info("LoadingTimeInterceptor: postHandle() Exiting....." + ((HandlerMethod)handler).getMethod().getName());
+    }
+    
+
     
 
 
