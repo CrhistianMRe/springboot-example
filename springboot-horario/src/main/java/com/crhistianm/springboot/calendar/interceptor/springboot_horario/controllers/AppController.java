c2b4d604b8daaa8b28559997385a8de59f890efc
CrhistianMRe <came.29meca@gmail.com>
2025-05-25 08:43:46 +0800

    feat(interceptor-h): add foo response entity method with mapping

--- springboot-horario/src/main/java/com/crhistianm/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
+++ springboot-horario/src/main/java/com/crhistianm/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -1,5 +1,20 @@
 package com.crhistianm.springboot.calendar.interceptor.springboot_horario.controllers;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo(){
+
+       Map<String, Object> data = Collections.singletonMap("Title", "Bienvenidos al sistema de atencion!");
+       return ResponseEntity.ok(data);
+    }
 
 }
