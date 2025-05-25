5c6aa00f1661d01c1b8af58d710eedb7d3a2b426
CrhistianMRe <came.29meca@gmail.com>
2025-05-25 09:23:34 +0800

    feat(interceptor-h): add response message when closed as json on method

--- springboot-horario/src/main/java/com/crhistianm/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
+++ springboot-horario/src/main/java/com/crhistianm/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
@@ -1,11 +1,16 @@
 package com.crhistianm.springboot.calendar.interceptor.springboot_horario.interceptors;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,6 +32,7 @@ public class CalendarInterceptor implements HandlerInterceptor{
         Calendar calendar = Calendar.getInstance();
 
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        System.out.println(hour);
 
         if(hour >= open && hour < close){
             StringBuilder message = new StringBuilder("Bienvenidos al horario de atencion a clientes!");
@@ -44,6 +50,24 @@ public class CalendarInterceptor implements HandlerInterceptor{
             return true;
         }
     
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> data = new HashMap<>();
+        StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atencion");
+        message.append("por favor visitenos desde las ");
+        message.append(open);
+        message.append(" y las ");
+        message.append(close);
+        message.append(" hrs. Gracias!");
+
+        data.put("message", message.toString());
+        data.put("date", new Date());
+
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(mapper.writeValueAsString(data));
+
+
+
         return false;
     }
 
