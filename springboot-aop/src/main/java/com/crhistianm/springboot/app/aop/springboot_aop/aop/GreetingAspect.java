74c7452b47b37188087acafb55c209f6ce9c9c70
CrhistianMRe <came.29meca@gmail.com>
2025-05-25 13:42:26 +0800

    feat(aop): add GreetingAspect base class

---
+++ springboot-aop/src/main/java/com/crhistianm/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -0,0 +1,13 @@
+package com.crhistianm.springboot.app.aop.springboot_aop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * GreetingAspect
+ */
+
+@Aspect
+public class GreetingAspect {
+
+    
+}
