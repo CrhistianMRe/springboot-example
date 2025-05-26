8a388e64a6d78b5b3903a555d0e877b7ff33577a
CrhistianMRe <came.29meca@gmail.com>
2025-05-26 10:57:24 +0800

    feat(aop): add Order 1 annotation for GreetingFooAspect class

--- springboot-aop/src/main/java/com/crhistianm/springboot/app/aop/springboot_aop/aop/GreetingFooAspect.java
+++ springboot-aop/src/main/java/com/crhistianm/springboot/app/aop/springboot_aop/aop/GreetingFooAspect.java
@@ -8,12 +8,14 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
  * GreetingFooAspect
  */
 
+@Order(1)
 @Component
 @Aspect
 public class GreetingFooAspect {
