92ae5ed0f741ade015a611d3644c2c2d104b1a90
CrhistianMRe <came.29meca@gmail.com>
2025-05-26 12:09:32 +0800

    feat(aop): add GreetingServicePointcuts class

---
+++ springboot-aop/src/main/java/com/crhistianm/springboot/app/aop/springboot_aop/aop/GreetingServicePointcuts.java
@@ -0,0 +1,15 @@
+package com.crhistianm.springboot.app.aop.springboot_aop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * GreetingServicePointcuts
+ */
+
+@Aspect
+@Component
+public class GreetingServicePointcuts {
+
+    
+}
