4a539cd49ab89de1762739873e4a8b2e739de91e
CrhistianMRe <came.29meca@gmail.com>
2025-05-27 20:17:32 +0800

    feat(jpa): add access mutator methods and attributes for class Person

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Person.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Person.java
@@ -10,5 +10,51 @@ import jakarta.persistence.Table;
 @Table(name="persons")
 public class Person {
 
+    private Long id;
+    private String name;
+    private String lastname;
+    private String programmingLanguage;
+
+    public Person (Long id, String name, String lastname, String programmingLanguage){
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public Person (){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
 }
