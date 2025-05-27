4806f846bb2f5c166f2324265f50f1058f2f3703
CrhistianMRe <came.29meca@gmail.com>
2025-05-27 20:00:34 +0800

    feat(jpa): add entities package and base class Person with entity and table annotation

---
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Person.java
@@ -0,0 +1,14 @@
+package com.crhistianm.springboot.jpa.springboot_jpa.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * Person
+ */
+@Entity
+@Table(name="persons")
+public class Person {
+
+    
+}
