12d01512595affa4c91023b70c53efdb56dea79c
CrhistianMRe <came.29meca@gmail.com>
2025-05-28 09:40:45 +0800

    feat(jpa): inject PersonRepository to main class and call findAll method to print

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,12 +1,20 @@
 package com.crhistianm.springboot.jpa.springboot_jpa;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
+import com.crhistianm.springboot.jpa.springboot_jpa.repositories.PersonRepository;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner{
 
+    @Autowired
+    private PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
@@ -15,6 +23,13 @@ public class SpringbootJpaApplication implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         
+        List<Person> persons = (List<Person>)repository.findAll();
+
+        persons.stream().forEach(person -> {
+                System.out.println(person);
+        }); 
+
     }
+
 
 }
