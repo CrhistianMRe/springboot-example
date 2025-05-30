36683a3eac65191e77bc2780c608aff4ec7a887c
CrhistianMRe <came.29meca@gmail.com>
2025-05-30 21:17:11 +0800

    feat(jpa): add new personalized method with @Query annotation

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -2,6 +2,7 @@ package com.crhistianm.springboot.jpa.springboot_jpa.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
@@ -12,6 +13,9 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
     List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
 
     
 
