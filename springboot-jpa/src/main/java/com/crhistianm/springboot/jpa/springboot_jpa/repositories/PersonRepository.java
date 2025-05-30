788c97a9f20a2052b313c180c53c1ac47b85a29d
CrhistianMRe <came.29meca@gmail.com>
2025-05-30 21:05:37 +0800

    feat(jpa): add programming language query method to PersonRepository interface

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.crhistianm.springboot.jpa.springboot_jpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
@@ -8,6 +10,10 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
  * PersonRepository
  */
 public interface PersonRepository extends CrudRepository<Person, Long>{
+
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    
 
     
 }
