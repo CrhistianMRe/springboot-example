7872c214ddd08514fc382b1f495bfaecd7c8f11a
CrhistianMRe <came.29meca@gmail.com>
2025-05-30 21:07:10 +0800

    feat(jpa): update current active query method on main

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -23,7 +23,8 @@ public class SpringbootJpaApplication implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         
-        List<Person> persons = (List<Person>)repository.findAll();
+        //List<Person> persons = (List<Person>)repository.findAll();
+        List<Person> persons = (List<Person>)repository.findByProgrammingLanguage(null);
 
         persons.stream().forEach(person -> {
                 System.out.println(person);
