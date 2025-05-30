c3eea5debeac5d57bb707698b1b78dfb321b1510
CrhistianMRe <came.29meca@gmail.com>
2025-05-30 21:18:41 +0800

    feat(jpa): call buscarByProgrammingLanguage method with attributes

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -24,7 +24,7 @@ public class SpringbootJpaApplication implements CommandLineRunner{
     public void run(String... args) throws Exception {
         
         //List<Person> persons = (List<Person>)repository.findAll();
-        List<Person> persons = (List<Person>)repository.findByProgrammingLanguage(null);
+        List<Person> persons = (List<Person>)repository.buscarByProgrammingLanguage("Java", "Maria");
 
         persons.stream().forEach(person -> {
                 System.out.println(person);
