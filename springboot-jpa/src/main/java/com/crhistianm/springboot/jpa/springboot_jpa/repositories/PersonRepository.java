8648e9950f766fd36b973bdaa4c4241ad8828ec2
CrhistianMRe <came.29meca@gmail.com>
2025-06-01 21:12:28 +0800

    feat(jpa): add personalized query getFullNameById on PersonRepository interface

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -13,6 +13,9 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
  */
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
+    @Query("select concat(p.name, ' ', p.lastname) as fullname from Person p where p.id=?1")
+    String getFullNameById(Long id);
+
     @Query("select p.name from Person p where p.id=?1")
     String getNameById(Long id);
 
