9153a192f344b8b1d4e84898e5e3876910fbac79
CrhistianMRe <came.29meca@gmail.com>
2025-06-03 07:45:34 +0800

    feat(jpa): comment previous personalizedQueriesBetween implementations on main

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -44,13 +44,15 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 
     @Transactional(readOnly = true)
     public void personalizedQueriesBetween(){
-        System.out.println("============= consultas por rangos ============= ");
+
+
+        /*System.out.println("============= consultas por rangos ============= ");
         List<Person> persons = repository.findByIdBetween(2L, 5L);
         persons.forEach(System.out::println);
 
         System.out.println("============= consultas por nombres ============= ");
         persons = repository.findByNameBetween("J", "Q");
-        persons.forEach(System.out::println);
+        persons.forEach(System.out::println);*/
 
     }
 
