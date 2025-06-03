6e37403527891b5050f25ae6818be3dd73d4e62f
CrhistianMRe <came.29meca@gmail.com>
2025-06-03 20:52:07 +0800

    feat(jpa): add getPersonNameLength query method on PersonRepository interface

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -14,6 +14,10 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
  */
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
+
+    @Query("select p.name, length(p.name) from Person p")
+    public List<Object[]> getPersonNameLength();
+
     @Query("select count(p) from Person p")
     Long totalPerson();
 
