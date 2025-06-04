f559ac135f9cdb73e2b0edb4b427438ac0c78ffc
CrhistianMRe <came.29meca@gmail.com>
2025-06-04 21:46:24 +0800

    feat(jpa): add getResumeAggregationFunction personalized query method to PersonRepository interface

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -14,6 +14,9 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
  */
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
+    @Query("select min(p.id), max(p.id), sum(p.id), avg(length(p.name)), count(p.id) from Person p")
+    public Object getResumeAggregationFunction();
+
     @Query("select min(length(p.name)) from Person p")
     public Integer getMinLengthName();
 
