8d2b157fc05a523540a179ef6175979bc2cfffa2
CrhistianMRe <came.29meca@gmail.com>
2025-06-05 14:36:22 +0800

    feat(jpa): add getPersonsByIds personalized query method in PersonRepository interface

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -14,6 +14,9 @@ import com.crhistianm.springboot.jpa.springboot_jpa.entities.Person;
  */
 public interface PersonRepository extends CrudRepository<Person, Long>{
 
+    @Query("select p from Person p where p.id in (1,2,5)")
+    public List<Person> getPersonsByIds();
+
     @Query("select p.name, length(p.name) from Person p where length(p.name)=(select min(length(p.name)) from Person p)")
     public List<Object[]> getShorterName();
 
