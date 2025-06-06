2f4790216037176ef93ed6574e7c01c84be825e6
CrhistianMRe <came.29meca@gmail.com>
2025-06-06 14:20:20 +0800

    feat(jpa): move prePersist method from Person entity to Audit entity

--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Audit.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Audit.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
 
 /**
  * Audit
@@ -14,5 +15,10 @@ public class Audit {
     @Column(name = "create_at")
     private LocalDateTime createAt;
 
+    @PrePersist
+    public void prePersist(){
+        System.out.println("Evento del ciclo de ciclo de vida del entity pre persist");
+        this.createAt = LocalDateTime.now();
+    }
     
 }
--- springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Person.java
+++ springboot-jpa/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa/entities/Person.java
@@ -40,11 +40,6 @@ public class Person {
         this.programmingLanguage = programmingLanguage;
     }
 
-    @PrePersist
-    public void prePersist(){
-        System.out.println("Evento del ciclo de ciclo de vida del entity pre persist");
-        this.createAt = LocalDateTime.now();
-    }
 
     @PreUpdate
     public void preUpdate(){
