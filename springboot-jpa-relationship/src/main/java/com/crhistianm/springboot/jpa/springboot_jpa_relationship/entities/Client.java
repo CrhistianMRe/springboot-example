4c82d950efb79b433dc5c1cbefdb31bb6928324d
CrhistianMRe <came.29meca@gmail.com>
2025-06-07 10:16:20 +0800

    feat(jpa-rel): add base client entity class

---
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,15 @@
+package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * Client
+ */
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    
+}
