4625c19184975b1b049d227f5c77100e5cf02344
CrhistianMRe <came.29meca@gmail.com>
2025-06-07 10:20:42 +0800

    feat(jpa-rel): add base invoice entity class

---
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -0,0 +1,14 @@
+package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * Invoice
+ */
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+
+    
+}
