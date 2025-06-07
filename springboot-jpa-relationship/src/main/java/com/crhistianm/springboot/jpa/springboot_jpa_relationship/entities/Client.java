42831eaf4ad3ae45112f536eb733b6149a646311
CrhistianMRe <came.29meca@gmail.com>
2025-06-07 10:49:07 +0800

    feat(jpa-rel): add id variable to Client entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -1,6 +1,10 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
 
+import org.springframework.data.annotation.Id;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
 /**
@@ -11,5 +15,9 @@ import jakarta.persistence.Table;
 @Table(name = "clients")
 public class Client {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
