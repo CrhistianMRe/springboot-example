1a5004bbbbfe48e15180d92aaa6642dafff82b55
CrhistianMRe <came.29meca@gmail.com>
2025-06-08 14:18:51 +0800

    feat(jpa-rel): add CommandLineRunner implementation with run override method on main

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,19 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 
 }
