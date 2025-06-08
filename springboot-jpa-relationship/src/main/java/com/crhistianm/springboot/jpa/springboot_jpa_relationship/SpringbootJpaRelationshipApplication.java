d6b7b764731691f0d4fe28f5cd101f0e34db245b
CrhistianMRe <came.29meca@gmail.com>
2025-06-08 14:24:46 +0800

    feat(jpa-rel): add ClientRepository injection on main

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,11 +1,18 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.crhistianm.springboot.jpa.springboot_jpa_relationship.repositories.ClientRepository;
+
 @SpringBootApplication
 public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
+
+
+    @Autowired
+    private ClientRepository clientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
