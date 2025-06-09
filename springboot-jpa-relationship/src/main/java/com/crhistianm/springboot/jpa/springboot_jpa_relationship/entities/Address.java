2574cc60320b7f4799e737a13acfb4668edc6aa4
CrhistianMRe <came.29meca@gmail.com>
2025-06-09 10:07:39 +0800

    feat(jpa-rel): add access mutator methods for all variables on Address entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
@@ -21,5 +21,25 @@ public class Address {
 
     private Integer number;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
 }
