d3ff9dc871afc8e253b8b53108fcdd98211c7461
CrhistianMRe <came.29meca@gmail.com>
2025-06-09 10:02:49 +0800

    feat(jpa-rel): add base Address entity mapped to table

---
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
@@ -0,0 +1,14 @@
+package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * Address
+ */
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    
+}
