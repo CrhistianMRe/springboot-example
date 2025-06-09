ed831fa94aa74dcb185f85d3afba5b764e40bb4d
CrhistianMRe <came.29meca@gmail.com>
2025-06-09 10:04:25 +0800

    feat(jpa-rel): add id variable to Address entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
@@ -1,6 +1,9 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 /**
@@ -9,6 +12,10 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "addresses")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     
 }
