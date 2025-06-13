7223246a744c845dec09a87dab6fc33ccb673439
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 13:30:09 +0800

    feat(jpa-rel): add hashCode override method on Address entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
@@ -50,8 +50,13 @@ public class Address {
         this.number = number;
     }
 
-
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
     @Override
     public boolean equals(Object obj) {
