f333e2991174d168bbb23f093c01518dc65283b6
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 19:26:10 +0800

    feat(jpa-rel): add findOne personalized query method on ClientRepository interface

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
@@ -17,4 +17,7 @@ public interface ClientRepository extends CrudRepository<Client, Long>{
 
     @Query("select c from Client c left join fetch c.invoices where c.id=?1")
     Optional<Client> findOneWithInvoices(Long id);
+
+    @Query("select c from Client c left join fetch c.invoices left join fetch c.addresses where c.id=?1")
+    Optional<Client> findOne(Long id);
 }
