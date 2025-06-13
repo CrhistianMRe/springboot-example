7dce148f154a729542a5f1cd2771b177d3ef7e4f
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 19:21:41 +0800

    feat(jpa-rel): comment addresses temporarily to toString method on Client entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -103,7 +103,9 @@ public class Client {
 
     @Override
     public String toString() {
-        return "{id=" + id + ", name=" + name + ", lastname=" + lastname +", invoices=" + invoices + ", addresses=" + addresses + "}";
+        return "{id=" + id + ", name=" + name + ", lastname=" + lastname +", invoices=" + invoices + 
+           // ", addresses=" + addresses +
+            "}";
     }
 
 
