3b1538399985d4722b3ffaa29ff52dab1a049656
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 13:22:29 +0800

    feat(jpa-rel): add override method equals for verifying id object on Address entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Address.java
@@ -50,6 +50,26 @@ public class Address {
         this.number = number;
     }
 
+
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Address other = (Address) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
     @Override
     public String toString() {
         return "{id=" + id + ", street=" + street + ", number=" + number+"}";
