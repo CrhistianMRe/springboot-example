5f3b72eb782916c29b0d0a27b473dfe17ba55081
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 16:50:44 +0800

    feat(jpa-rel): remove client from toString method on Invoice entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -69,7 +69,7 @@ public class Invoice {
 
     @Override
     public String toString() {
-        return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
+        return "{id=" + id + ", description=" + description + ", total=" + total + "}";
     }
 
 
