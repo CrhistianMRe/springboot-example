09e7c24aba6d1f2eda7cd1303df2071dd9917bcd
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 19:41:46 +0800

    refactor(jpa-rel): remove unnecessary imports on Client entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -2,9 +2,7 @@ package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
 
 
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -14,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
