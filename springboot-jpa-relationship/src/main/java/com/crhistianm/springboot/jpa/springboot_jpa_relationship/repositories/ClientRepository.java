cf1f1efa7edc716bf347f21bc382623b04db9d2b
CrhistianMRe <came.29meca@gmail.com>
2025-06-13 19:18:21 +0800

    feat(jpa-rel): replace inner join for left join to findOneWithAddresses personalized query method on ClientRepository interface

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
@@ -12,7 +12,7 @@ import com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities.Client
  */
 public interface ClientRepository extends CrudRepository<Client, Long>{
 
-    @Query("select c from Client c join fetch c.addresses where c.id=?1")
+    @Query("select c from Client c left join fetch c.addresses where c.id=?1")
     Optional<Client> findOneWithAddresses(Long id);
 
     @Query("select c from Client c join fetch c.invoices where c.id=?1")
