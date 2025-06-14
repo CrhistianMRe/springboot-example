3697c7a49ad29ab15994617feb6ac72dea579c83
CrhistianMRe <came.29meca@gmail.com>
2025-06-14 13:56:24 +0800

    feat(jpa-rel): add access/mutator methods for all attributes on Client entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/ClientDetails.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/ClientDetails.java
@@ -1,5 +1,6 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,9 +18,28 @@ public class ClientDetails {
     private boolean premium;
     private Integer points;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    
+    public void setPremium(boolean premium) {
+        this.premium = premium;
+    }
 
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
     
 }
