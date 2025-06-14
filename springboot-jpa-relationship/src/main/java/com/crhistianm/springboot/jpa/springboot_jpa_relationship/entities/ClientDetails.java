a55530334784965ccf8f2025aaf67a28be3f6855
CrhistianMRe <came.29meca@gmail.com>
2025-06-14 13:07:47 +0800

    feat(jpa-rel): add id attribute to ClientDetails entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/ClientDetails.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/ClientDetails.java
@@ -1,11 +1,22 @@
 package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "client_details")
 public class ClientDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    
 
     
 }
