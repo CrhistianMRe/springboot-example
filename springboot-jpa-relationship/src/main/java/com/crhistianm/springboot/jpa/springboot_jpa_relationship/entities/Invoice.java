a08b010bf32737bd3e59da274760691bb31f9bfc
CrhistianMRe <came.29meca@gmail.com>
2025-06-14 12:24:12 +0800

    feat(jpa-rel): add hashCode override method on Invoice entity

--- springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -68,6 +68,15 @@ public class Invoice {
     }
 
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((total == null) ? 0 : total.hashCode());
+        return result;
+    }
 
     @Override
     public boolean equals(Object obj) {
