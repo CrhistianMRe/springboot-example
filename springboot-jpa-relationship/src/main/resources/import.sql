38be4430f0a1cb0af3f6b1f9de5f0d8a7ca43faa
CrhistianMRe <came.29meca@gmail.com>
2025-06-15 15:27:46 +0800

    feat(jpa-rel): add new student inserts on import.sql

--- springboot-jpa-relationship/src/main/resources/import.sql
+++ springboot-jpa-relationship/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO clients (name, lastname) VALUES ('Maria', 'Roe');
 INSERT INTO clients (name, lastname) VALUES ('Pepe', 'Doe');
 
+
+INSERT INTO students (name, lastname) VALUES ('Nacho', 'Goe');
+INSERT INTO students (name, lastname) VALUES ('Pepa', 'Gon');
