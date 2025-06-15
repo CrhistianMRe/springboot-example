76bb0f204297dc90110f476cc7d20086549e5d23
CrhistianMRe <came.29meca@gmail.com>
2025-06-15 14:41:27 +0800

    feat(jpa-rel): add StudentRepository interface to repositories package

---
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.crhistianm.springboot.jpa.springboot_jpa_relationship.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    
+}
+
