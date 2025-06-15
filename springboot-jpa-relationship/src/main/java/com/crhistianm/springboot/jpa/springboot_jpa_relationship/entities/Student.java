a175ffa7383534c66d58e602a6d0ba4f491b2bcd
CrhistianMRe <came.29meca@gmail.com>
2025-06-15 13:37:14 +0800

    feat(jpa-rel): add base Student entity class mapped to table on entities package

---
+++ springboot-jpa-relationship/src/main/java/com/crhistianm/springboot/jpa/springboot_jpa_relationship/entities/Student.java
@@ -0,0 +1,12 @@
+package com.crhistianm.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+
+    
+}
