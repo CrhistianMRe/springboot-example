6160ca4821e3f762f7886644cb93b5f71b3db0d8
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 11:53:29 +0800

    feat(crud): add RequestMapping to ProductController

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crhistianm.springboot.app.springboot_crud.entities.Product;
 import com.crhistianm.springboot.app.springboot_crud.services.ProductService;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
