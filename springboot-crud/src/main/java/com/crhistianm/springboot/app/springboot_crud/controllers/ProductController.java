b41726a2824cd1be7837f9ed261c1dc6815ff9aa
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 11:50:32 +0800

    feat(crud): add list method returning findAll method from productService with mapping

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.crhistianm.springboot.app.springboot_crud.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.crhistianm.springboot.app.springboot_crud.entities.Product;
 import com.crhistianm.springboot.app.springboot_crud.services.ProductService;
 
 @RestController
@@ -12,5 +16,9 @@ public class ProductController {
     private ProductService productService;
 
 
+    @GetMapping
+    public List<Product> list(){
+        return productService.findAll();
+    }
     
 }
