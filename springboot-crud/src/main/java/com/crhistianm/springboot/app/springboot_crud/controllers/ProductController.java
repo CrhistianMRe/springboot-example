dd5be16d577a19af38f38f99610cbf6e4fa15f3c
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 13:34:44 +0800

    feat(crud): add Post method create to save product on ProductController

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +37,9 @@ public class ProductController {
         }
         return ResponseEntity.notFound().build();
     }
-    
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
 }
