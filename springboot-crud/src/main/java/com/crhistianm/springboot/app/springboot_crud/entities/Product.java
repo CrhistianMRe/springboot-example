48a38fd1a3abad5078fb9c65bcb84ad532dbc52e
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 10:16:00 +0800

    feat(crud): add Product entity mapped to products Table on db

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Product.java
@@ -0,0 +1,13 @@
+package com.crhistianm.springboot.app.springboot_crud.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+
+
+    
+}
