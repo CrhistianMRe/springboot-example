593378e40631464a3d340c4ba19072622607d258
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 13:28:16 +0800

    feat(crud): add view method to retrieve a product with exception return if not found on ProductController

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.crhistianm.springboot.app.springboot_crud.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,14 @@ public class ProductController {
     @GetMapping
     public List<Product> list(){
         return productService.findAll();
+    }
+
+    public ResponseEntity<?> view(@PathVariable Long id){
+        Optional<Product> productOptional = productService.findById(id);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
     }
     
 }
