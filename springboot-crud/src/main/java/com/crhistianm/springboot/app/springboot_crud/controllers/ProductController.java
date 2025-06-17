72e9a7b31c7f7066c3bca3701845a9ac7727324a
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 11:45:03 +0800

    feat(crud): add base ProductController as RestController

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.crhistianm.springboot.app.springboot_crud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crhistianm.springboot.app.springboot_crud.services.ProductService;
+
+@RestController
+public class ProductController {
+
+
+
+    
+}
