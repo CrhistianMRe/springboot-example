8283ce135ad64b8b19f9257f23d40353b3b7516e
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 10:46:57 +0800

    feat(crud): add ProductService interface with crud methods

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.crhistianm.springboot.app.springboot_crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.Product;
+
+public interface ProductService {
+
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    void delete(Product product);
+    
+}
