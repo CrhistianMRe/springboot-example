2a299a470398065c0d77cf2a3e15568a43e929a3
CrhistianMRe <came.29meca@gmail.com>
2025-06-17 13:44:32 +0800

    feat(crud): add Delete delete method to delete by received id with exception return on ProductController

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,16 @@ public class ProductController {
     public ResponseEntity<Product> create(@RequestBody Product product){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Product product = new Product();
+        product.setId(id);
+        Optional<Product> productOptional = productService.delete(product);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
