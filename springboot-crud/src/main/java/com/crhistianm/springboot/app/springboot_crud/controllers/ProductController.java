412c2e1af7007c8ae28a8f765d1a5b3a607d3dc9
CrhistianMRe <came.29meca@gmail.com>
2025-06-18 10:09:47 +0800

    feat(crud): add update Put method on ProductController to get product to update it on db

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,12 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> create(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+        product.setId(id);
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
