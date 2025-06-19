39b3b9a155d7944e06c1cb729258277d6a54f3c3
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 15:37:28 +0800

    feat(crud-ssec): add ManyToMany annotation relationship with intermediary table matching db on User entity

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/User.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/User.java
@@ -29,6 +29,13 @@ public class User {
 
     private String password;
 
+    @ManyToMany
+    @JoinTable(
+        name = "users_roles",
+        joinColumns = @JoinColumn (name ="user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"),
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id, role_id"})}
+    )
     private List<Role> roles;
 
 
