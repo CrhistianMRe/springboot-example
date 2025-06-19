f57e19a9466619b15306c1f17bc864384707fd4b
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 13:34:54 +0800

    feat(crud-ssec): add Role entity attributes with access/mutator methods

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
@@ -1,12 +1,40 @@
 package com.crhistianm.springboot.app.springboot_crud.entities;
 
+import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
+
