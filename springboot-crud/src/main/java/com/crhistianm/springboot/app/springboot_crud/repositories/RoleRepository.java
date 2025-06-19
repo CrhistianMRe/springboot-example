2d4ede0c5bc298f9a355d0ab26cb939422537fe4
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 15:40:51 +0800

    feat(crud-ssec): add base RoleRepository

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.crhistianm.springboot.app.springboot_crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long>{
+
+    
+}
