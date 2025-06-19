b2728c9bfe9b731bd51d78b3c00f20650265f398
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 15:41:04 +0800

    feat(crud-ssec): add base UserRepository

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.crhistianm.springboot.app.springboot_crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    
+}
