e2bb73bf403bfb5965dadbcfa872e7460698a8d7
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 12:50:22 +0800

    feat(crud): add table roles DDL

--- springboot-crud/src/main/resources/dbcrud.sql
+++ springboot-crud/src/main/resources/dbcrud.sql
@@ -18,11 +18,18 @@ CREATE TABLE IF NOT EXISTS users(
      
 );
 
+CREATE TABLE IF NOT EXISTS roles(
+    role_id bigint NOT NULL,
+    name varchar(45) NOT NULL
+);
+
 ALTER TABLE products ADD PRIMARY KEY (product_id);
 ALTER TABLE users ADD PRIMARY KEY (user_id);
+ALTER TABLE roles ADD PRIMARY KEY (role_id);
 
 ALTER TABLE products MODIFY product_id bigint NOT NULL AUTO_INCREMENT;
 ALTER TABLE users MODIFY user_id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE roles MODIFY role_id bigint NOT NULL AUTO_INCREMENT;
 
 
 
