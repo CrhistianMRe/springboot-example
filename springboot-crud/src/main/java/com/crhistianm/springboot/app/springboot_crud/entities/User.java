3d67c285db5fe2d9fec1dd872449c7c317e60cb5
CrhistianMRe <came.29meca@gmail.com>
2025-06-19 13:32:06 +0800

    feat(crud-ssec): add base User entity mapped to table

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/User.java
@@ -0,0 +1,12 @@
+package com.crhistianm.springboot.app.springboot_crud.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    
+}
