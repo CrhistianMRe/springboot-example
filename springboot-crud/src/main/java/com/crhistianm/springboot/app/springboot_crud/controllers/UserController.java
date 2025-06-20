16f189adfff6feafc900e2af1dc80aa85a96b5cd
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 12:24:09 +0800

    feat(crud-ssec): add base UserController with Mapping

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.crhistianm.springboot.app.springboot_crud.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    
+}
