c14de3552c4397fb7363b5e0b20a9abbea040bbf
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 13:42:38 +0800

    fix(crud-ssec): replace wrong import

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/User.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/User.java
@@ -3,7 +3,6 @@ package com.crhistianm.springboot.app.springboot_crud.entities;
 
 import java.util.List;
 
-import org.springframework.data.annotation.Transient;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,6 +14,7 @@ import jakarta.persistence.JoinColumns;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
