ccb8a76fc191c102b6ecb5531f3bc50478b141c0
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 11:51:13 +0800

    feat(crud-ssec): add transactional User save method implementation on UserServiceImpl

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/UserServiceImpl.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/UserServiceImpl.java
@@ -1,13 +1,30 @@
 package com.crhistianm.springboot.app.springboot_crud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.crhistianm.springboot.app.springboot_crud.entities.Role;
 import com.crhistianm.springboot.app.springboot_crud.entities.User;
+import com.crhistianm.springboot.app.springboot_crud.repositories.RoleRepository;
+import com.crhistianm.springboot.app.springboot_crud.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public List<User> findAll() {
@@ -16,9 +33,21 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
-        // TODO Auto-generated method stub
-        return null;
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+        optionalRoleUser.ifPresent(roles::add);
+
+        if(user.isAdmin()){
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
     }
     
 }
