a8784db72db680a7d030b47a7c22e9392b677202
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 10:59:02 +0800

    feat(crud-ssec): add base SpringSecurityConfig class

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.crhistianm.springboot.app.springboot_crud.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringSecurityConfig {
+    
+    
+    
+}
