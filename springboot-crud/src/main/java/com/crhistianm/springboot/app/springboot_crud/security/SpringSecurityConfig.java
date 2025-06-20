3a00d346390eb0022811b0aa89d205b2940efefe
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 13:44:22 +0800

    feat(crud-ssec): add filterChain Bean component to apply it to specified endpoint

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/SpringSecurityConfig.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.crhistianm.springboot.app.springboot_crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -12,6 +15,21 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests( (authz) -> authz
+                //set endpoint public to all GET,POST,PUT,DELETE, etc.. 
+                .requestMatchers("/users").permitAll()
+                //Any other endpoint request requires auth
+                .anyRequest().authenticated())
+                //Disable token csrf which provides security
+                .csrf(config -> config.disable())
+                //Set session stateless to manage all auth stuff on token
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+       
     }
 
 }
