853c3f0b1dd762451318df02034fcddb59de6616
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 10:56:10 +0800

    feat(crud-ssec): add reserved query method findByName on RoleRepository

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/RoleRepository.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/RoleRepository.java
@@ -1,10 +1,13 @@
 package com.crhistianm.springboot.app.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.crhistianm.springboot.app.springboot_crud.entities.Role;
 
 public interface RoleRepository extends CrudRepository<Role, Long>{
 
+    Optional<Role> findByName(String name);
     
 }
