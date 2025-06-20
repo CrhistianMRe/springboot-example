1a967864a02c64fc24f454a34e2f419c33641b9d
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 12:33:44 +0800

    feat(crud-ssec): add POST create method to default endpoint

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/UserController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/UserController.java
@@ -3,7 +3,11 @@ package com.crhistianm.springboot.app.springboot_crud.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,11 @@ public class UserController {
     @GetMapping
     public List<User> list(){
         return userService.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
     
 }
