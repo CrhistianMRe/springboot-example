a80e3284daf26aca7a36d95dc33c07a06b69be41
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 10:43:10 +0800

    feat(crud-ssec): add base methods to UserService interface

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/UserService.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/UserService.java
@@ -1,7 +1,13 @@
 package com.crhistianm.springboot.app.springboot_crud.services;
 
+import java.util.List;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.User;
 
 public interface UserService {
 
+    List<User> findAll();
+
+    User save(User user);
     
 }
