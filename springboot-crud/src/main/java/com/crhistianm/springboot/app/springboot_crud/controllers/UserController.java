6cd85caf2d89dc50d29a27f524b47702b19d8ad0
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 12:28:29 +0800

    feat(crud-ssec): add GET list method to default endpoint

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/UserController.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/controllers/UserController.java
@@ -1,11 +1,25 @@
 package com.crhistianm.springboot.app.springboot_crud.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.User;
+import com.crhistianm.springboot.app.springboot_crud.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> list(){
+        return userService.findAll();
+    }
     
 }
