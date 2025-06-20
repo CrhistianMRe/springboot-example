daa2a53c30b747b0df08905565012fe3a8236eea
CrhistianMRe <came.29meca@gmail.com>
2025-06-20 11:40:54 +0800

    feat(crud-ssec): add UserServiceImpl base service implementation

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.crhistianm.springboot.app.springboot_crud.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User save(User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
