38ea4fd4b2f26a8efb51291b0db20868053011af
CrhistianMRe <came.29meca@gmail.com>
2025-06-21 14:39:19 +0800

    feat(crud-ssec): add instance recognition for inverse relationship attributes on matching entities

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
@@ -63,5 +63,32 @@ public class Role {
         this.users = users;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((users == null) ? 0 : users.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (users == null) {
+            if (other.users != null)
+                return false;
+        } else if (!users.equals(other.users))
+            return false;
+        return true;
+    }
+
+
+    
 }
 
