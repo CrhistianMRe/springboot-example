4d09e3a7b90dd8d6f9f36137af44aeeb92597614
CrhistianMRe <came.29meca@gmail.com>
2025-06-21 13:03:52 +0800

    feat(crud-ssec): add reverse relationship as users attribute on Role entity

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/entities/Role.java
@@ -1,11 +1,15 @@
 package com.crhistianm.springboot.app.springboot_crud.entities;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,7 +24,11 @@ public class Role {
     @Column(unique = true)
     private String name;
 
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
     public Role(){
+        this.users = new ArrayList<>();
     }
 
     public Role(String name){
@@ -41,6 +49,14 @@ public class Role {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
 }
