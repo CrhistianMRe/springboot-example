366d0d1f1f7b9e091e463ea233f8ef4e8045c19f
CrhistianMRe <came.29meca@gmail.com>
2025-06-21 13:50:06 +0800

    feat(crud-ssec): add base JpaUserDetailsService class

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/JpaUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.crhistianm.springboot.app.springboot_crud.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService{
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+
+    
+}
