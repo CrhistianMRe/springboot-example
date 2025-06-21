2933a2f3c5ead035cce6ec0e3166cd2389aa3f20
CrhistianMRe <came.29meca@gmail.com>
2025-06-21 14:22:42 +0800

    feat(crud-ssec): add JpaUserDetailsService loadUserByUsername transactional method implementantion
    NOTE: This last class JpaUserDetailsService uses UserDetailsService interface. It does a query and gets the username from it for creating an GrantedAuthority List which will be all roles from the db; After that it creates a Spring Framework Security User with the required attributes and authorities or roles

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/JpaUserDetailsService.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/services/JpaUserDetailsService.java
@@ -1,16 +1,56 @@
 package com.crhistianm.springboot.app.springboot_crud.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crhistianm.springboot.app.springboot_crud.entities.User;
+import com.crhistianm.springboot.app.springboot_crud.repositories.UserRepository;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService{
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> userOptional = userRepository.findByUsername(username);
+
+        if(userOptional.isEmpty()){
+            throw new UsernameNotFoundException(String.format("Username %s no existe en la db", username));
+        }
+
+        User user = userOptional.orElseThrow();
+
+
+        //Create from db roles list to GrantedAuthority list 
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+            .map(role -> new SimpleGrantedAuthority(role.getName()))
+            .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), 
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                authorities);
+
+
     }
 
     
