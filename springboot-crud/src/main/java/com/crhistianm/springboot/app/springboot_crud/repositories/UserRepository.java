c33520050f71b192bf11bd4d0f8ea47f444c4df5
CrhistianMRe <came.29meca@gmail.com>
2025-06-21 13:37:42 +0800

    feat(crud-ssec): add existsByUsername query method on UserRepository

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/UserRepository.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import com.crhistianm.springboot.app.springboot_crud.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    boolean existsByUsername(String username);
     
 }
