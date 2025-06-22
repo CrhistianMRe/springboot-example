264cb33789530821bfda81a31b0ccbe4dd6aa2ca
CrhistianMRe <came.29meca@gmail.com>
2025-06-22 21:27:18 +0800

    fix(crud-ssec): add service validation to IsInDatabaseValidation

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/validations/IsInDatabaseValidation.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/validations/IsInDatabaseValidation.java
@@ -1,18 +1,23 @@
 package com.crhistianm.springboot.app.springboot_crud.validations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.crhistianm.springboot.app.springboot_crud.services.ProductService;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+@Component
 public class IsInDatabaseValidation implements ConstraintValidator<IsInDatabase, String> {
 
     @Autowired
     private ProductService service;
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(service == null){
+            return true;
+        }
         return !service.existsBySku(value);
     }
 
