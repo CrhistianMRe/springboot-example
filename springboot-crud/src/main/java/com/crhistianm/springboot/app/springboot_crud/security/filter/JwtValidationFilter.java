5082260067d7b427ef25ac5a317197496975cdc5
CrhistianMRe <came.29meca@gmail.com>
2025-06-22 21:05:30 +0800

    feat(crud-ssec): add JwtValidationFilter filter class which will validate if the token is authentic

---
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/filter/JwtValidationFilter.java
@@ -0,0 +1,93 @@
+package com.crhistianm.springboot.app.springboot_crud.security.filter;
+
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.crhistianm.springboot.app.springboot_crud.security.SimpleGrantedAuthorityJsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.lang.Arrays;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import static com.crhistianm.springboot.app.springboot_crud.security.TokenJwtConfig.*;
+
+/**
+ * JwtValidationFilter
+ */
+public class JwtValidationFilter extends BasicAuthenticationFilter{
+
+
+    public JwtValidationFilter(AuthenticationManager authenticationManager ) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+            String header = request.getHeader(HEADER_AUTHORIZATION);
+
+            //The header has to start with the prefix
+            if(header == null || !header.startsWith(PREFIX_TOKEN)){
+                chain.doFilter(request, response);
+                return;
+            }
+            //Remove prefix from token
+            String token = header.replace(PREFIX_TOKEN, "");
+
+            try {
+                //Verify token with secret key and get claims from it
+                Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+                //Get username from claims
+                String username = claims.getSubject();
+                //Get authorities which will be the roles
+                Object authoritiesClaims = claims.get("authorities");
+
+                //Create a collection to convert from roles attribute
+                Collection<? extends GrantedAuthority> authorities = Arrays.asList(
+                        new ObjectMapper()
+                        .addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityJsonCreator.class)
+                        .readValue(authoritiesClaims.toString().getBytes(), SimpleGrantedAuthority[].class));
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                chain.doFilter(request, response);
+            } catch (JwtException e) {
+                Map<String, String> body = new HashMap<>();
+
+
+                body.put("error", e.getMessage());
+                body.put("message", "El token JWT es invalido!");
+
+                response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.setContentType(CONTENT_TYPE);
+
+
+            }
+
+
+    }
+
+
+
+    
+}
