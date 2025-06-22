eabeb4d5399734366ed2fb291d4c644e46abe48b
CrhistianMRe <came.29meca@gmail.com>
2025-06-22 15:29:14 +0800

    feat(crud-ssec): add implementation of JwtAuthenticationFilter to SpringSecurityConfig filterChain method

--- springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/SpringSecurityConfig.java
+++ springboot-crud/src/main/java/com/crhistianm/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -1,17 +1,30 @@
 package com.crhistianm.springboot.app.springboot_crud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.crhistianm.springboot.app.springboot_crud.security.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    //Method to get authentication manager through bean
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -26,6 +39,7 @@ public class SpringSecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
                 //Any other endpoint request requires auth
                 .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 //Disable token csrf which provides security
                 .csrf(config -> config.disable())
                 //Set session stateless to manage all auth stuff on token
